6026d1459c10ac731334620cdefad92f8904ab2f
Shankar Khatri <yes143.shankar@gmail.com>
2019-09-14 15:18:09 -0500

    Add marker content in marker click

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -3,6 +3,7 @@ export interface Mappable {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -25,6 +26,14 @@ export class CustomMap {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
